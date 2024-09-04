4525f2d99ebd77ffd1c60a4d8f4ae9037524c8a1
Danil Makarov <makardan@fit.cvut.cz>
2024-09-05 00:18:46 +0200

    Commit correct migrations

--- applifting-backend/migrations/1725327820709-init-schema.js
+++ applifting-backend/migrations/1725327820709-init-schema.js
@@ -4,25 +4,31 @@ module.exports = class InitSchema1725327820709 {
     name = 'InitSchema1725327820709'
 
     async up(queryRunner) {
-        await queryRunner.query(`CREATE TABLE "comment_votes" ("voteId" uuid NOT NULL DEFAULT uuid_generate_v4(), "ipAddress" character varying NOT NULL, "value" integer NOT NULL DEFAULT '0', "comment_id" uuid, CONSTRAINT "PK_7b3547bfeae2fbcfb4707a12a02" PRIMARY KEY ("voteId"))`);
-        await queryRunner.query(`CREATE TABLE "comments" ("commentId" uuid NOT NULL DEFAULT uuid_generate_v4(), "author" character varying NOT NULL, "content" character varying NOT NULL, "postedAt" TIMESTAMP NOT NULL DEFAULT now(), "score" integer NOT NULL DEFAULT '0', "article_id" uuid, CONSTRAINT "PK_b302f2e474ce2a6cbacd7981aa5" PRIMARY KEY ("commentId"))`);
-        await queryRunner.query(`CREATE TABLE "tenants" ("tenantId" uuid NOT NULL DEFAULT uuid_generate_v4(), "apiKey" character varying NOT NULL, "name" character varying NOT NULL, "password" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "lastUsedAt" TIMESTAMP DEFAULT now(), CONSTRAINT "UQ_30dec5cd2d1f58a2682a9c77bb8" UNIQUE ("apiKey"), CONSTRAINT "PK_5d1f2d0d0b5f5c5e1720082ebbd" PRIMARY KEY ("tenantId"))`);
+        await queryRunner.query(`CREATE TABLE "comment_votes" ("voteId" uuid NOT NULL DEFAULT uuid_generate_v4(), "ipAddress" character varying NOT NULL, "value" integer NOT NULL DEFAULT '0', "comment_id" uuid, "tenant_id" uuid, CONSTRAINT "PK_7b3547bfeae2fbcfb4707a12a02" PRIMARY KEY ("voteId"))`);
+        await queryRunner.query(`CREATE TABLE "comments" ("commentId" uuid NOT NULL DEFAULT uuid_generate_v4(), "author" character varying NOT NULL, "content" character varying NOT NULL, "postedAt" TIMESTAMP NOT NULL DEFAULT now(), "score" integer NOT NULL DEFAULT '0', "article_id" uuid, "tenant_id" uuid, CONSTRAINT "PK_b302f2e474ce2a6cbacd7981aa5" PRIMARY KEY ("commentId"))`);
+        await queryRunner.query(`CREATE TABLE "images" ("imageId" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "url" character varying NOT NULL, "mimeType" character varying NOT NULL, "articleArticleId" uuid, "tenantTenantId" uuid, CONSTRAINT "REL_a4145c976b7c58095294caaefe" UNIQUE ("articleArticleId"), CONSTRAINT "REL_66e8d16c2f5df6024d6accde23" UNIQUE ("tenantTenantId"), CONSTRAINT "PK_ec459f275c64a5f69513745e0c8" PRIMARY KEY ("imageId"))`);
         await queryRunner.query(`CREATE TABLE "articles" ("articleId" uuid NOT NULL DEFAULT uuid_generate_v4(), "title" character varying NOT NULL, "perex" character varying NOT NULL, "content" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "lastUpdatedAt" TIMESTAMP DEFAULT now(), "tenant_id" uuid, CONSTRAINT "PK_709f0216840494e847a430f57b7" PRIMARY KEY ("articleId"))`);
-        await queryRunner.query(`CREATE TABLE "images" ("imageId" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "url" character varying NOT NULL, "mimeType" character varying NOT NULL, "articleArticleId" uuid, CONSTRAINT "REL_a4145c976b7c58095294caaefe" UNIQUE ("articleArticleId"), CONSTRAINT "PK_ec459f275c64a5f69513745e0c8" PRIMARY KEY ("imageId"))`);
+        await queryRunner.query(`CREATE TABLE "tenants" ("tenantId" uuid NOT NULL DEFAULT uuid_generate_v4(), "apiKey" character varying NOT NULL, "name" character varying NOT NULL, "password" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "lastUsedAt" TIMESTAMP DEFAULT now(), CONSTRAINT "UQ_30dec5cd2d1f58a2682a9c77bb8" UNIQUE ("apiKey"), CONSTRAINT "PK_5d1f2d0d0b5f5c5e1720082ebbd" PRIMARY KEY ("tenantId"))`);
         await queryRunner.query(`ALTER TABLE "comment_votes" ADD CONSTRAINT "FK_1b41b98c56a06654513bffc1274" FOREIGN KEY ("comment_id") REFERENCES "comments"("commentId") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "comment_votes" ADD CONSTRAINT "FK_9083225a23314d231d68abbd518" FOREIGN KEY ("tenant_id") REFERENCES "tenants"("tenantId") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "comments" ADD CONSTRAINT "FK_e9b498cca509147e73808f9e593" FOREIGN KEY ("article_id") REFERENCES "articles"("articleId") ON DELETE CASCADE ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "articles" ADD CONSTRAINT "FK_fa70ef96fb4556d1fd280c7a461" FOREIGN KEY ("tenant_id") REFERENCES "tenants"("tenantId") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "comments" ADD CONSTRAINT "FK_bae41308a2185399e6bec738b56" FOREIGN KEY ("tenant_id") REFERENCES "tenants"("tenantId") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "images" ADD CONSTRAINT "FK_a4145c976b7c58095294caaefe3" FOREIGN KEY ("articleArticleId") REFERENCES "articles"("articleId") ON DELETE SET NULL ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "images" ADD CONSTRAINT "FK_66e8d16c2f5df6024d6accde23d" FOREIGN KEY ("tenantTenantId") REFERENCES "tenants"("tenantId") ON DELETE SET NULL ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "articles" ADD CONSTRAINT "FK_fa70ef96fb4556d1fd280c7a461" FOREIGN KEY ("tenant_id") REFERENCES "tenants"("tenantId") ON DELETE CASCADE ON UPDATE NO ACTION`);
     }
 
     async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "images" DROP CONSTRAINT "FK_a4145c976b7c58095294caaefe3"`);
         await queryRunner.query(`ALTER TABLE "articles" DROP CONSTRAINT "FK_fa70ef96fb4556d1fd280c7a461"`);
+        await queryRunner.query(`ALTER TABLE "images" DROP CONSTRAINT "FK_66e8d16c2f5df6024d6accde23d"`);
+        await queryRunner.query(`ALTER TABLE "images" DROP CONSTRAINT "FK_a4145c976b7c58095294caaefe3"`);
+        await queryRunner.query(`ALTER TABLE "comments" DROP CONSTRAINT "FK_bae41308a2185399e6bec738b56"`);
         await queryRunner.query(`ALTER TABLE "comments" DROP CONSTRAINT "FK_e9b498cca509147e73808f9e593"`);
+        await queryRunner.query(`ALTER TABLE "comment_votes" DROP CONSTRAINT "FK_9083225a23314d231d68abbd518"`);
         await queryRunner.query(`ALTER TABLE "comment_votes" DROP CONSTRAINT "FK_1b41b98c56a06654513bffc1274"`);
-        await queryRunner.query(`DROP TABLE "images"`);
-        await queryRunner.query(`DROP TABLE "articles"`);
         await queryRunner.query(`DROP TABLE "tenants"`);
+        await queryRunner.query(`DROP TABLE "articles"`);
+        await queryRunner.query(`DROP TABLE "images"`);
         await queryRunner.query(`DROP TABLE "comments"`);
         await queryRunner.query(`DROP TABLE "comment_votes"`);
     }
--- applifting-backend/migrations/1725357134426-insert-script.js
+++ applifting-backend/migrations/1725357134426-insert-script.js
@@ -34,34 +34,34 @@ module.exports = class InsertScript1725357134426 {
 
     // Insert comments for articles of tenant '5ba38c85-fb19-4ed3-99bd-8b34cf34705f'
     await queryRunner.query(`
-      INSERT INTO comments ("commentId", "author", "content", "postedAt", "score", "article_id")
+      INSERT INTO comments ("commentId", "author", "content", "postedAt", "score", "article_id", "tenant_id")
       VALUES
-      ('e2ff97ab-a79a-4e4d-8f01-15e51f902dbd', 'Author 1', 'Comment 1 for Article 1', '2024-09-03T10:01:00.000Z', 0, '0c475513-5ea4-40b9-8b32-500b0aa818af'),
-      ('21c23ff7-89a8-4adf-b043-ea2b075cae42', 'Author 2', 'Comment 2 for Article 1', '2024-09-03T10:02:00.000Z', 0, '0c475513-5ea4-40b9-8b32-500b0aa818af'),
-      ('b33b2b2b-ed5d-4ec8-b1e0-6628b11944e6', 'Author 1', 'Comment 1 for Article 2', '2024-09-03T10:03:00.000Z', 0, 'd8ec9cc7-18be-421f-8a51-33b3ecc33b42'),
-      ('93728f4e-4088-41e1-b4e9-bb5fe2144746', 'Author 2', 'Comment 2 for Article 2', '2024-09-03T10:04:00.000Z', 0, 'd8ec9cc7-18be-421f-8a51-33b3ecc33b42'),
-      ('8b88c8db-2b81-4b41-85d8-6430a361d136', 'Author 1', 'Comment 1 for Article 3', '2024-09-03T10:05:00.000Z', 0, 'a4b41a8a-1a11-4110-9e8b-e4a5bb539e00'),
-      ('7d0653f5-0b4c-40b2-ac5f-c174d7a45537', 'Author 2', 'Comment 2 for Article 3', '2024-09-03T10:06:00.000Z', 0, 'a4b41a8a-1a11-4110-9e8b-e4a5bb539e00'),
-      ('d603f54e-2e97-4b46-a313-59cf2c3e4930', 'Author 1', 'Comment 1 for Article 4', '2024-09-03T10:07:00.000Z', 0, '3b8c0c2c-4cac-46b9-91a8-5456e40b9551'),
-      ('3672fc97-7d59-4357-8272-b605f31de4f2', 'Author 2', 'Comment 2 for Article 4', '2024-09-03T10:08:00.000Z', 0, '3b8c0c2c-4cac-46b9-91a8-5456e40b9551'),
-      ('d89ba091-0424-4a24-a9b2-1b61266ba242', 'Author 1', 'Comment 1 for Article 5', '2024-09-03T10:09:00.000Z', 0, 'b52fecbb-6b7b-4198-8275-27aa4dea3855'),
-      ('184c3a86-5295-4b11-b143-04bf8bc55cbc', 'Author 2', 'Comment 2 for Article 5', '2024-09-03T10:10:00.000Z', 0, 'b52fecbb-6b7b-4198-8275-27aa4dea3855');
+      ('e2ff97ab-a79a-4e4d-8f01-15e51f902dbd', 'Author 1', 'Comment 1 for Article 1', '2024-09-03T10:01:00.000Z', 0, '0c475513-5ea4-40b9-8b32-500b0aa818af', '5ba38c85-fb19-4ed3-99bd-8b34cf34705f'),
+      ('21c23ff7-89a8-4adf-b043-ea2b075cae42', 'Author 2', 'Comment 2 for Article 1', '2024-09-03T10:02:00.000Z', 0, '0c475513-5ea4-40b9-8b32-500b0aa818af', '5ba38c85-fb19-4ed3-99bd-8b34cf34705f'),
+      ('b33b2b2b-ed5d-4ec8-b1e0-6628b11944e6', 'Author 1', 'Comment 1 for Article 2', '2024-09-03T10:03:00.000Z', 0, 'd8ec9cc7-18be-421f-8a51-33b3ecc33b42', '5ba38c85-fb19-4ed3-99bd-8b34cf34705f'),
+      ('93728f4e-4088-41e1-b4e9-bb5fe2144746', 'Author 2', 'Comment 2 for Article 2', '2024-09-03T10:04:00.000Z', 0, 'd8ec9cc7-18be-421f-8a51-33b3ecc33b42', '5ba38c85-fb19-4ed3-99bd-8b34cf34705f'),
+      ('8b88c8db-2b81-4b41-85d8-6430a361d136', 'Author 1', 'Comment 1 for Article 3', '2024-09-03T10:05:00.000Z', 0, 'a4b41a8a-1a11-4110-9e8b-e4a5bb539e00', '5ba38c85-fb19-4ed3-99bd-8b34cf34705f'),
+      ('7d0653f5-0b4c-40b2-ac5f-c174d7a45537', 'Author 2', 'Comment 2 for Article 3', '2024-09-03T10:06:00.000Z', 0, 'a4b41a8a-1a11-4110-9e8b-e4a5bb539e00', '5ba38c85-fb19-4ed3-99bd-8b34cf34705f'),
+      ('d603f54e-2e97-4b46-a313-59cf2c3e4930', 'Author 1', 'Comment 1 for Article 4', '2024-09-03T10:07:00.000Z', 0, '3b8c0c2c-4cac-46b9-91a8-5456e40b9551', '5ba38c85-fb19-4ed3-99bd-8b34cf34705f'),
+      ('3672fc97-7d59-4357-8272-b605f31de4f2', 'Author 2', 'Comment 2 for Article 4', '2024-09-03T10:08:00.000Z', 0, '3b8c0c2c-4cac-46b9-91a8-5456e40b9551', '5ba38c85-fb19-4ed3-99bd-8b34cf34705f'),
+      ('d89ba091-0424-4a24-a9b2-1b61266ba242', 'Author 1', 'Comment 1 for Article 5', '2024-09-03T10:09:00.000Z', 0, 'b52fecbb-6b7b-4198-8275-27aa4dea3855', '5ba38c85-fb19-4ed3-99bd-8b34cf34705f'),
+      ('184c3a86-5295-4b11-b143-04bf8bc55cbc', 'Author 2', 'Comment 2 for Article 5', '2024-09-03T10:10:00.000Z', 0, 'b52fecbb-6b7b-4198-8275-27aa4dea3855', '5ba38c85-fb19-4ed3-99bd-8b34cf34705f');
     `);
 
     // Insert comments for articles of tenant '5025ac03-21d7-414c-a1d9-ab786ba20ca0'
     await queryRunner.query(`
-      INSERT INTO comments ("commentId", "author", "content", "postedAt", "score", "article_id")
+      INSERT INTO comments ("commentId", "author", "content", "postedAt", "score", "article_id", "tenant_id")
       VALUES
-      ('ee7f9aeb-36d5-40b9-9fd5-61d5c5c974d3', 'Author A', 'Comment 1 for Article A', '2024-09-03T10:01:00.000Z', 0, 'cd18d973-e8b1-4f42-9adf-86519f88d9ec'),
-      ('c42927e7-9d5a-45e7-8fbe-cf8d2cf6b5c4', 'Author B', 'Comment 2 for Article A', '2024-09-03T10:02:00.000Z', 0, 'cd18d973-e8b1-4f42-9adf-86519f88d9ec'),
-      ('5b8e728d-9050-4bde-9b6e-9c07e25d842f', 'Author A', 'Comment 1 for Article B', '2024-09-03T10:03:00.000Z', 0, '34f4dfb7-04c4-424b-bd58-132ef8354af8'),
-      ('7f8e2d32-1a4b-4f24-a8b9-4fbff2566f25', 'Author B', 'Comment 2 for Article B', '2024-09-03T10:04:00.000Z', 0, '34f4dfb7-04c4-424b-bd58-132ef8354af8'),
-      ('cd8c3245-4319-4242-8cc3-c0f02398b64b', 'Author A', 'Comment 1 for Article C', '2024-09-03T10:05:00.000Z', 0, '67436be6-1d90-411e-9d71-529b64f7b32d'),
-      ('18af5b7e-9e2e-48b2-beb3-c687d8f853df', 'Author B', 'Comment 2 for Article C', '2024-09-03T10:06:00.000Z', 0, '67436be6-1d90-411e-9d71-529b64f7b32d'),
-      ('af83e137-0913-41d1-bd88-bf5f1b9dbe3a', 'Author A', 'Comment 1 for Article D', '2024-09-03T10:07:00.000Z', 0, 'f2fef1b7-bbdd-4d0b-9ef4-e4d2c0cb183d'),
-      ('1ea2d329-13d3-448d-9393-eae1b8f919b4', 'Author B', 'Comment 2 for Article D', '2024-09-03T10:08:00.000Z', 0, 'f2fef1b7-bbdd-4d0b-9ef4-e4d2c0cb183d'),
-      ('b61e99b4-1cb3-4f8c-bdc9-9fbb893647af', 'Author A', 'Comment 1 for Article E', '2024-09-03T10:09:00.000Z', 0, '231267b1-9189-4527-9866-5d7d20f9aeb8'),
-      ('78d3ea1e-44e5-4c3d-b3a7-9f8cb8c8d5e9', 'Author B', 'Comment 2 for Article E', '2024-09-03T10:10:00.000Z', 0, '231267b1-9189-4527-9866-5d7d20f9aeb8');
+      ('ee7f9aeb-36d5-40b9-9fd5-61d5c5c974d3', 'Author A', 'Comment 1 for Article A', '2024-09-03T10:01:00.000Z', 0, 'cd18d973-e8b1-4f42-9adf-86519f88d9ec', '5025ac03-21d7-414c-a1d9-ab786ba20ca0'),
+      ('c42927e7-9d5a-45e7-8fbe-cf8d2cf6b5c4', 'Author B', 'Comment 2 for Article A', '2024-09-03T10:02:00.000Z', 0, 'cd18d973-e8b1-4f42-9adf-86519f88d9ec', '5025ac03-21d7-414c-a1d9-ab786ba20ca0'),
+      ('5b8e728d-9050-4bde-9b6e-9c07e25d842f', 'Author A', 'Comment 1 for Article B', '2024-09-03T10:03:00.000Z', 0, '34f4dfb7-04c4-424b-bd58-132ef8354af8', '5025ac03-21d7-414c-a1d9-ab786ba20ca0'),
+      ('7f8e2d32-1a4b-4f24-a8b9-4fbff2566f25', 'Author B', 'Comment 2 for Article B', '2024-09-03T10:04:00.000Z', 0, '34f4dfb7-04c4-424b-bd58-132ef8354af8', '5025ac03-21d7-414c-a1d9-ab786ba20ca0'),
+      ('cd8c3245-4319-4242-8cc3-c0f02398b64b', 'Author A', 'Comment 1 for Article C', '2024-09-03T10:05:00.000Z', 0, '67436be6-1d90-411e-9d71-529b64f7b32d', '5025ac03-21d7-414c-a1d9-ab786ba20ca0'),
+      ('18af5b7e-9e2e-48b2-beb3-c687d8f853df', 'Author B', 'Comment 2 for Article C', '2024-09-03T10:06:00.000Z', 0, '67436be6-1d90-411e-9d71-529b64f7b32d', '5025ac03-21d7-414c-a1d9-ab786ba20ca0'),
+      ('af83e137-0913-41d1-bd88-bf5f1b9dbe3a', 'Author A', 'Comment 1 for Article D', '2024-09-03T10:07:00.000Z', 0, 'f2fef1b7-bbdd-4d0b-9ef4-e4d2c0cb183d', '5025ac03-21d7-414c-a1d9-ab786ba20ca0'),
+      ('1ea2d329-13d3-448d-9393-eae1b8f919b4', 'Author B', 'Comment 2 for Article D', '2024-09-03T10:08:00.000Z', 0, 'f2fef1b7-bbdd-4d0b-9ef4-e4d2c0cb183d', '5025ac03-21d7-414c-a1d9-ab786ba20ca0'),
+      ('b61e99b4-1cb3-4f8c-bdc9-9fbb893647af', 'Author A', 'Comment 1 for Article E', '2024-09-03T10:09:00.000Z', 0, '231267b1-9189-4527-9866-5d7d20f9aeb8', '5025ac03-21d7-414c-a1d9-ab786ba20ca0'),
+      ('78d3ea1e-44e5-4c3d-b3a7-9f8cb8c8d5e9', 'Author B', 'Comment 2 for Article E', '2024-09-03T10:10:00.000Z', 0, '231267b1-9189-4527-9866-5d7d20f9aeb8', '5025ac03-21d7-414c-a1d9-ab786ba20ca0');
     `);
   }
 
