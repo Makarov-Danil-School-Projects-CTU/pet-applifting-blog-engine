b8992c53f5f5c05041a89dffb14eb89b9134133f
Danil Makarov <makardan@fit.cvut.cz>
2024-09-05 00:19:42 +0200

    Create current tenant middleware

---
+++ applifting-backend/src/middlewares/current-tenant.middleware.ts
@@ -0,0 +1,39 @@
+import { ExecutionContext, Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { Tenant } from '../entities/tenant.entity';
+import { TenantService } from '../tenant/tenant.service';
+
+// Rewrite a basic Request type from Express to add a currentUser field
+declare global {
+  namespace Express {
+    interface Request {
+      tenant?: Tenant;
+    }
+  }
+}
+
+@Injectable()
+export class CurrentTenantMiddleware implements NestMiddleware {
+  constructor(private tenantService: TenantService) {}
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    const apiKey = req.headers['x-api-key'] as string;
+
+    if (apiKey) {
+      const tenant = await this.tenantService.findTenantByApiKey(apiKey);
+      req.tenant = tenant;
+    }
+
+    // If using GraphQL, ensure the request is properly handled
+    if (req.body && req.body.operationName) {
+      // Attach tenant to the GraphQL context
+      const context = req.body.context || {};
+      context.tenant = req.tenant;
+      req.body.context = context;
+    }
+
+    next();
+  }
+}
