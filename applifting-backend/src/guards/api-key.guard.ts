f92dfe54d023329f616f23c685c6e649a35292b0
Danil Makarov <makardan@fit.cvut.cz>
2024-09-05 00:17:55 +0200

    Refactor guards

--- applifting-backend/src/guards/api-key.guard.ts
+++ applifting-backend/src/guards/api-key.guard.ts
@@ -6,16 +6,9 @@ import {
 } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 
-// This is a custom guard for handling GraphQL requests
-// For websockets req is different. Because of that we have 2 versions for HTTP and WebSockets
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
-    /**
-     * For GraphQL, the execution context in NestJS uses GqlExecutionContext
-     * rather than the default HTTP context. Because of that, we cannot use
-     * context.switchToHttp(). It's undefined
-     */
     const ctx = GqlExecutionContext.create(context);
     const req = ctx.getContext().req;
     const apiKey = req.headers['x-api-key'];
