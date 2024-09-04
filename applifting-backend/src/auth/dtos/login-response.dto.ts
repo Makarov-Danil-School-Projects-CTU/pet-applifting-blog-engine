5fd64e9ea91fd7027840f22dfc7c355945689306
Danil Makarov <makardan@fit.cvut.cz>
2024-09-05 00:18:21 +0200

    Refactor auth dtos

--- applifting-backend/src/auth/dtos/login-response.dto.ts
+++ applifting-backend/src/auth/dtos/login-response.dto.ts
@@ -1,25 +1,21 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNumber, IsString } from "class-validator";
 
 export class LoginResponseDto {
   @ApiProperty({
     description: 'The access token used for authentication',
     example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
   })
-  @IsString()
   access_token: string;
   
   @ApiProperty({
     description: 'The time in seconds until the access token expires',
     example: 3600,
   })
-  @IsNumber()
   expires_in: number;
 
   @ApiProperty({
     description: 'The type of token being returned',
     example: 'bearer',
   })
-  @IsString()
   token_type: string;
 }
--- applifting-backend/src/auth/dtos/login-user.dto.ts
+++ applifting-backend/src/auth/dtos/login-user.dto.ts
@@ -1,12 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class LoginUserDto {
-  @ApiProperty({description: "The username of the created tenant"})
+  @ApiProperty({ description: 'The username of the created tenant' })
   @IsString()
+  @IsNotEmpty()
+  @Length(5, 20)
   username: string;
 
-  @ApiProperty({description: "The password of the created tenant"})
+  @ApiProperty({ description: 'The password of the created tenant' })
   @IsString()
+  @IsNotEmpty()
+  @Length(6, 50)
   password: string;
 }
