f327f9b5715acd04f7215673b65a7e8d5b36bbab
Danil Makarov <makardan@fit.cvut.cz>
2024-09-05 00:17:43 +0200

    Edit resolvers

--- applifting-backend/src/graphql/resolvers/article.resolver.ts
+++ applifting-backend/src/graphql/resolvers/article.resolver.ts
@@ -1,8 +1,18 @@
-import { UseGuards } from '@nestjs/common';
-import { Args, ID, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Req, UseGuards } from '@nestjs/common';
+import {
+  Args,
+  Context,
+  GqlExecutionContext,
+  ID,
+  Mutation,
+  Query,
+  Resolver,
+} from '@nestjs/graphql';
 
 import { ArticleService } from '../../article/article.service';
+import { CurrentTenantGraphQL } from '../../decorators/current-tenant.decorator';
 import { Article } from '../../entities/article.entity';
+import { Tenant } from '../../entities/tenant.entity';
 import { AccessTokenGuard } from '../../guards/access-token.guard';
 import { ApiKeyGuard } from '../../guards/api-key.guard';
 import { CreateArticleInput } from '../inputs/create-article.input';
@@ -16,24 +26,28 @@ export class ArticleResolver {
 
   // Resolver for getting all articles
   @Query(() => [Article], { name: 'getArticles' })
-  async getArticles(): Promise<Article[]> {
-    return this.articleService.getAllArticles();
+  async getArticles(
+    @CurrentTenantGraphQL() tenant: Tenant,
+  ): Promise<Article[]> {
+    return this.articleService.getAllArticles(tenant.tenantId);
   }
 
   // Resolver for getting a specific article by ID
   @Query(() => Article, { name: 'getArticle' })
   async getArticle(
     @Args('articleId', { type: () => ID }) articleId: string,
+    @CurrentTenantGraphQL() tenant: Tenant,
   ): Promise<Article> {
-    return this.articleService.getArticleById(articleId);
+    return this.articleService.getArticleById(articleId, tenant.tenantId);
   }
 
   // Resolver for creating a new article
   @Mutation(() => Article)
   async createArticle(
     @Args('input') input: CreateArticleInput,
+    @CurrentTenantGraphQL() tenant: Tenant,
   ): Promise<Article> {
-    return this.articleService.createArticle(input);
+    return this.articleService.createArticle(input, tenant.tenantId);
   }
 
   // Resolver for updating an existing article
@@ -41,15 +55,17 @@ export class ArticleResolver {
   async updateArticle(
     @Args('articleId', { type: () => ID }) articleId: string,
     @Args('input') input: UpdateArticleInput,
+    @CurrentTenantGraphQL() tenant: Tenant,
   ): Promise<Article> {
-    return this.articleService.updateArticle(articleId, input);
+    return this.articleService.updateArticle(articleId, input, tenant.tenantId);
   }
 
   // Resolver for deleting an article
   @Mutation(() => Article)
   async deleteArticle(
     @Args('articleId', { type: () => ID }) articleId: string,
+    @CurrentTenantGraphQL() tenant: Tenant,
   ): Promise<Article> {
-    return this.articleService.deleteArticle(articleId);
+    return this.articleService.deleteArticle(articleId, tenant.tenantId);
   }
 }
--- applifting-backend/src/graphql/resolvers/comment.resolver.ts
+++ applifting-backend/src/graphql/resolvers/comment.resolver.ts
@@ -9,28 +9,32 @@ import {
 import { PubSub } from 'graphql-subscriptions';
 
 import { CommentService } from '../../comment/comment.service';
+import { CurrentTenantGraphQL } from '../../decorators/current-tenant.decorator';
 import { Comment } from '../../entities/comment.entity';
-import { AccessTokenGuardWebsockets } from '../../guards/access-token-websockets.guard';
-import { ApiKeyGuardWebsockets } from '../../guards/api-key-websockets.guard';
+import { Tenant } from '../../entities/tenant.entity';
+import { AccessTokenGuard } from '../../guards/access-token.guard';
+import { ApiKeyGuard } from '../../guards/api-key.guard';
 import { CreateCommentInput } from '../inputs/create-comment.input';
 import { VoteCommentInput } from '../inputs/vote-comment.input';
 
 const pubSub = new PubSub(); // GraphQL PubSub instance
 
 @Resolver(() => Comment)
-// This is a custom guard for handling GraphQL requests
-// For websockets req is different. Because of that we have 2 versions for HTTP and WebSockets
-@UseGuards(AccessTokenGuardWebsockets)
-@UseGuards(ApiKeyGuardWebsockets)
 export class CommentResolver {
   constructor(private readonly commentService: CommentService) {}
 
   // here is createComment mutation and following subscription
   @Mutation(() => Comment)
+  @UseGuards(AccessTokenGuard)
+  @UseGuards(ApiKeyGuard)
   async createComment(
     @Args('input') input: CreateCommentInput,
+    @CurrentTenantGraphQL() tenant: Tenant,
   ): Promise<Comment> {
-    const newComment = await this.commentService.createComment(input);
+    const newComment = await this.commentService.createComment(
+      input,
+      tenant.tenantId,
+    );
 
     // Publish to GraphQL subscription after creating a comment
     pubSub.publish('commentAdded', { commentAdded: newComment });
@@ -50,14 +54,18 @@ export class CommentResolver {
 
   // here is voteComment mutation and following subscription
   @Mutation(() => Comment)
+  @UseGuards(AccessTokenGuard)
+  @UseGuards(ApiKeyGuard)
   async voteComment(
     @Args('input') input: VoteCommentInput,
     @Context() context: any,
+    @CurrentTenantGraphQL() tenant: Tenant,
   ): Promise<Comment> {
     const ipAddress = context.req.ip;
     const updatedComment = await this.commentService.voteOnComment(
       input,
       ipAddress,
+      tenant.tenantId
     );
 
     // Publish to GraphQL subscription after voting on a comment
